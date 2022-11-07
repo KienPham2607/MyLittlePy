48bd7b48880d60d555937800803dd58bae327583
KienPham2607 <kienptgch200815@fpt.edu.vn>
2022-11-07 19:21:48 +0700

    init

---
+++ PyThon/GetStarted/full_name.py
@@ -0,0 +1,7 @@
+first_name = "kien"
+last_name = "pham"
+full_name = f"{first_name} {last_name}"
+my_name = "{} {}".format(last_name, first_name)
+print(full_name.title())
+print(f"{first_name} {last_name.upper()}")
+print(my_name)
