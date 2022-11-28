dc127c912fb58bc36fd3b54348e9694d056ba920
KienPham2607 <kienptgch200815@fpt.edu.vn>
2022-11-28 09:24:26 +0700

    done

--- PyThon/Ex/Exercise01.py
+++ PyThon/Ex/Exercise01.py
@@ -16,7 +16,7 @@ class Person:
         self.__address = new_address
 
     def toString(self) -> str:
-        return f"{self.__name.title()}\({self.__address.title()}\)"
+        return f"{self.__name.title()}({self.__address.title()})"
 
 
 class Student(Person):
@@ -35,8 +35,72 @@ class Student(Person):
         self.__grades.append(grade)
 
     def printGrade(self) -> None:
-        # Research how to crreate string array
-        pass
+        for i in range(0, self.__numCourses):
+            print(f"{self.__courses[i]}({self.__grades[i]})")
 
     def getAverageGrade(self) -> float:
-        return sum(self.__grades) / self.__grades.itemsize
+
+        return sum(self.__grades) / len(self.__grades)
+
+
+class Teacher(Person):
+    def __init__(self, name: str, address: str):
+        super().__init__(name, address)
+        self.__numCourses = 0
+        self.__courses = []
+
+    def toString(self) -> str:
+        return f"Teacher: {super().toString()}"
+
+    def addCourse(self, course: str) -> bool:
+        if course in self.__courses:
+            return False
+        else:
+            self.__courses.append(course)
+            self.__numCourses += 1
+            return True
+
+    def removeCourse(self, course: str) -> bool:
+        if course not in self.__courses:
+            return False
+        else:
+            self.__courses.remove(course)
+            self.__numCourses -= 1
+            return True
+
+    def getCourse(self):
+        print(self.__courses)
+
+
+a = Student("Ngoc Anh", "Trai Thong")
+print("Student to string")
+print(a.toString())
+
+print("\nReAssgin address")
+a.setAddress("Ninh Binh")
+print(a.toString())
+
+print("\nAddcourse and print grade")
+a.addCourseGrade("Data structure and Algorithms", 8)
+a.addCourseGrade("SDLC", 7)
+a.printGrade()
+print(f"Average grade: {a.getAverageGrade()}")
+
+print('\nTeacher')
+b = Teacher("Trung Kien", "Ninh Binh")
+print(f"Teacher to string: {b.toString()}")
+if b.addCourse("DSA"):
+    print("Added successfully")
+else:
+    print("Addition failed")
+
+b.addCourse("SDLC")
+b.addCourse("Project web")
+b.getCourse()
+# Add duplicate
+if b.removeCourse("DSA"):
+    print("removed successfully")
+else:
+    print("remove failed")
+
+b.getCourse()
